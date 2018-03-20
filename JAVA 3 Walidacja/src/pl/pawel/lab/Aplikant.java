32d100cc9968a8ee4819b01eeef802b1dc973c92
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2018-03-20 17:41:41 +0100

    Walidowanie daty nie działa

--- JAVA 3 Walidacja/src/pl/pawel/lab/Aplikant.java
+++ JAVA 3 Walidacja/src/pl/pawel/lab/Aplikant.java
@@ -1,23 +1,43 @@
 package pl.pawel.lab;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Aplikant {
-	private String firstName="";
-	private String surname="";
-	private String pesel="";
-	private String nip="";
-	private String bankAccountNumber="";
-	private Date dateOfBirth=new Date();
-	private String gender="";
-	
+	public enum Gender {
+		M, F, UNDEFINED;
+		@Override
+		public String toString() {
+			if (this == F) {
+				return "F";
+			} else {
+				return "M";
+			}
+		}
+	}
+
+	private ArrayList<Integer> peselData = new ArrayList<Integer>();
+
+	private String firstName = "";
+	private String surname = "";
+	private String pesel = "";
+	private String nip = "";
+	private String bankAccountNumber = "";
+	private Date dateOfBirth = new Date();
+	private Gender gender = Gender.UNDEFINED;
+
 	@Override
 	public String toString() {
-		return firstName+" "+surname+" "+pesel+" "+nip+" "+bankAccountNumber+" "+dateOfBirth.toString()+" "+gender;
+		return firstName + " " + surname + " " + pesel + " " + nip + " " + bankAccountNumber + " "
+				+ dateOfBirth.toString() + " " + gender.toString();
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -50,19 +70,38 @@ public class Aplikant {
 	}
 
 	public void setPesel(String pesel) {
-		Pattern p=Pattern.compile("^[0-9]{11}$");
-		Matcher m=p.matcher(pesel);
-		int kontrolna=0;
-		int[] wielomian= {9,7,3,1,9,7,3,1,9,7};
-		
+		Pattern p = Pattern.compile("^[0-9]{11}$");
+		Matcher m = p.matcher(pesel);
+		int kontrolna = 0;
+		int[] wielomian = { 9, 7, 3, 1, 9, 7, 3, 1, 9, 7 };
+
 		for (int i = 0; i < wielomian.length; i++) {
-			kontrolna+=wielomian[i]*Character.getNumericValue(pesel.toCharArray()[i]);
+			kontrolna += wielomian[i] * Character.getNumericValue(pesel.toCharArray()[i]);
 		}
-		
-		if (!m.matches() || kontrolna%10!=Character.getNumericValue(pesel.toCharArray()[pesel.length()-1])) {
+
+		if (!m.matches() || kontrolna % 10 != Character.getNumericValue(pesel.toCharArray()[pesel.length() - 1])) {
 			return;
 		}
+
 		this.pesel = pesel;
+
+		if (Integer.parseInt(pesel.substring(2, 4)) < 20) {
+			peselData.add(Integer.parseInt("19" + pesel.substring(0, 2)));
+			peselData.add(Integer.parseInt(pesel.substring(2, 4)));
+		} else if (Integer.parseInt(pesel.substring(2, 4)) < 40) {
+			peselData.add(Integer.parseInt("20" + pesel.substring(0, 2)));
+			peselData.add(Integer.parseInt(pesel.substring(2, 4)) - 20);
+		} else if (Integer.parseInt(pesel.substring(2, 4)) < 60) {
+			peselData.add(Integer.parseInt("21" + pesel.substring(0, 2)));
+			peselData.add(Integer.parseInt(pesel.substring(2, 4)) - 40);
+		} else if (Integer.parseInt(pesel.substring(2, 4)) < 80) {
+			peselData.add(Integer.parseInt("22" + pesel.substring(0, 2)));
+			peselData.add(Integer.parseInt(pesel.substring(2, 4)) - 60);
+		} else {
+			peselData.add(Integer.parseInt("18" + pesel.substring(0, 2)));
+			peselData.add(Integer.parseInt(pesel.substring(2, 4)) - 60);
+		}
+
 	}
 
 	public String getNip() {
@@ -70,16 +109,16 @@ public class Aplikant {
 	}
 
 	public void setNip(String nip) {
-		Pattern p=Pattern.compile("^[0-9]{10}$"); 
-		Matcher m=p.matcher(nip);
-		int kontrolna=0;
-		int[] wielomian= {6,5,7,2,3,4,5,6,7};
-		
+		Pattern p = Pattern.compile("^[0-9]{10}$");
+		Matcher m = p.matcher(nip);
+		int kontrolna = 0;
+		int[] wielomian = { 6, 5, 7, 2, 3, 4, 5, 6, 7 };
+
 		for (int i = 0; i < wielomian.length; i++) {
-			kontrolna+=wielomian[i]*Character.getNumericValue(pesel.toCharArray()[i]);
+			kontrolna += wielomian[i] * Character.getNumericValue(pesel.toCharArray()[i]);
 		}
-		
-		if (!m.matches() || kontrolna%10!=Character.getNumericValue(nip.toCharArray()[nip.length()-1])) {
+
+		if (!m.matches() || kontrolna % 10 != Character.getNumericValue(nip.toCharArray()[nip.length() - 1])) {
 			return;
 		}
 		this.nip = nip;
@@ -90,8 +129,8 @@ public class Aplikant {
 	}
 
 	public void setBankAccountNumber(String bankAccountNumber) {
-		Pattern p=Pattern.compile("^[0-9]{26}$");
-		Matcher m=p.matcher(bankAccountNumber);
+		Pattern p = Pattern.compile("^[0-9]{26}$");
+		Matcher m = p.matcher(bankAccountNumber);
 		if (!m.matches()) {
 			return;
 		}
@@ -103,16 +142,31 @@ public class Aplikant {
 	}
 
 	public void setDateOfBirth(Date dateOfBirth) {
+		LocalDate c=dateOfBirth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		int y = c.getYear();
+		int m = c.getMonthValue();
+		int d = c.getDayOfMonth();
+		System.out.println(y);
+		System.out.println(m);
+		System.out.println(d);
+		if (peselData.get(0) == y && peselData.get(1) == m && peselData.get(2) == d) {
+			this.dateOfBirth = dateOfBirth;
+		}else {
+			return;
+		}
 		
-		this.dateOfBirth = dateOfBirth;
 	}
 
-	public String getGender() {
+	public Gender getGender() {
 		return gender;
 	}
 
-	public void setGender(String gender) {
-		this.gender = gender;
+	public void setGender(Gender gender) {
+		if (pesel.toCharArray()[pesel.length() - 2] % 2 == 0 && gender.equals(Gender.F)) {
+			this.gender = gender;
+		} else if (pesel.toCharArray()[pesel.length() - 2] % 2 == 1 && gender.equals(Gender.M)) {
+			this.gender = gender;
+		}
 	}
 
 }
--- JAVA 3 Walidacja/src/pl/pawel/lab/Main.java
+++ JAVA 3 Walidacja/src/pl/pawel/lab/Main.java
@@ -1,13 +1,25 @@
 package pl.pawel.lab;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import pl.pawel.lab.Aplikant.Gender;
+
 public class Main {
 	public static void main(String[] args) {
 		Aplikant a=new Aplikant();
-		System.out.println(a.toString());
 		a.setFirstName("Pawel");
 		a.setSurname("Pankiewicz");
 		a.setPesel("97102603235");
 		a.setNip("1234563218");
+		SimpleDateFormat sdf=new SimpleDateFormat("YYYY/MM/DD");
+		try {
+			a.setDateOfBirth(sdf.parse("1997/10/26"));
+		} catch (ParseException e) {
+			System.err.println("Błąd parsowania daty");
+			e.printStackTrace();
+		}
+		a.setGender(Gender.M);
 		System.out.println(a.toString());
 	}
 	
